61432ba3d29c6800dd3cb7bea47778eeeafab9b3
yijie4188 <yijie4188@gmail.com>
2023-07-08 19:54:47 +0800

    Provide notify mechanism for update timeline card when card is edit

--- src/store.ts
+++ src/store.ts
@@ -1,10 +1,35 @@
-import { useEffect, useReducer } from 'react'
+import { useCallback, useEffect, useReducer, useSyncExternalStore } from 'react'
 
 interface Store {
   [DateKey: string]: { content: string }[]
 }
 
 export let store: Store
+
+const listeners: Map<string, Function[]> = new Map()
+
+function subscribeStore(dk: string, listener: Function) {
+  if (!listeners.has(dk)) {
+    listeners.set(dk, [])
+  }
+  const ls = listeners.get(dk)!
+  ls.push(listener)
+  return () => {
+    const idx = ls.indexOf(listener)
+    if (idx !== -1) {
+      ls.splice(idx, 1)
+    } else {
+      console.warn('listener not found')
+    }
+  }
+}
+
+function notifyStore(dk: string) {
+  const ls = listeners.get(dk)
+  if (ls) {
+    ls.forEach(l => l(store[dk]))
+  }
+}
 
 try {
   store = JSON.parse(localStorage.getItem('store') ?? '{}')
@@ -20,10 +45,14 @@ try {
 export function setGridData(dateKey: string, data: { content: string }[]) {
   store[dateKey] = data
   localStorage.setItem('store', JSON.stringify(store))
+  notifyStore(dateKey)
 }
 
 export function useDateData(date = new Date()) {
   const dk = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}`
+
+  const subscribeStoreWrap = useCallback((lis: Function) => subscribeStore(dk, lis), [dk])
+  const dateData = useSyncExternalStore(subscribeStoreWrap, () => store[dk] ?? [])
 
   const [a, d] = useReducer((
     state: { content: string }[],
@@ -41,9 +70,9 @@ export function useDateData(date = new Date()) {
         setGridData(dk, action.data)
         return action.data
     }
-  }, store[dk] ?? [])
+  }, dateData)
   useEffect(() => {
-    d({ type: 'upd', data: store[dk] ?? [] })
-  }, [dk])
+    d({ type: 'upd', data: dateData })
+  }, [dateData])
   return [a, d] as const
 }
