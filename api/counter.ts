a27f4f2906bc1c006196f36cdb9d7b413f6cd6d4
yijie4188 <yijie4188@gmail.com>
2023-07-12 01:04:47 +0800

    Counter to test upstash connect

---
+++ api/counter.ts
@@ -0,0 +1,18 @@
+import { Redis } from '@upstash/redis'
+
+import { defineJSONHandler } from './utils/defineHandler.ts'
+
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL as string,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN as string
+})
+
+export default defineJSONHandler<{ isReset: string }>(async ({ query }) => {
+  const isReset = ['true', '1', ''].includes(query.isReset)
+
+  if (isReset) {
+    await redis.set('counter', '0')
+    return { c: 0 }
+  }
+  return { c: await redis.incr('counter') }
+})
