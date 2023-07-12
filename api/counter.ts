82d2a6a855acc2f709bd7b2eb1f9b39cb3cb7f6b
yijie4188 <yijie4188@gmail.com>
2023-07-12 13:44:58 +0800

    Refactor getRedis as util function file

--- api/counter.ts
+++ api/counter.ts
@@ -1,11 +1,7 @@
-import { Redis } from '@upstash/redis'
-
 import { defineJSONHandler } from '#svr/utils/defineHandler.js'
+import { getRedis } from '#svr/utils/getRedis.js'
 
-const redis = new Redis({
-  url: process.env.UPSTASH_REDIS_REST_URL as string,
-  token: process.env.UPSTASH_REDIS_REST_TOKEN as string
-})
+const redis = getRedis()
 
 export default defineJSONHandler<{ isReset: string }>(async ({ query }) => {
   const isReset = ['true', '1', ''].includes(query.isReset)
