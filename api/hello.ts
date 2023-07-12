cffaea56633679376106732bca5accc9f11cee23
yijie4188 <yijie4188@gmail.com>
2023-07-12 11:15:44 +0800

    Rollback import path with `.js` extension

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,4 @@
-import { defineJSONHandler } from './utils/_defineHandler'
+import { defineJSONHandler } from './utils/_defineHandler.js'
 
 export default defineJSONHandler(function () {
   return { name: 'John Doe' }
