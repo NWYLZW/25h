096939bac4a95175b5736360dfd8feb2b25f88fb
yijie4188 <yijie4188@gmail.com>
2023-07-12 23:30:23 +0800

    Make redis as single instance in serverless function

--- svr/utils/getRedis.ts
+++ svr/utils/getRedis.ts
@@ -1,8 +1,10 @@
 import { Redis } from '@upstash/redis'
 
+const redis = new Redis({
+  url: process.env.UPSTASH_REDIS_REST_URL,
+  token: process.env.UPSTASH_REDIS_REST_TOKEN
+})
+
 export function getRedis() {
-  return new Redis({
-    url: process.env.UPSTASH_REDIS_REST_URL,
-    token: process.env.UPSTASH_REDIS_REST_TOKEN
-  })
+  return redis
 }
