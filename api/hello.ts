57ee6db80e51fd81cbc73fbdd314a5ba205e6e7c
yijie4188 <yijie4188@gmail.com>
2023-07-12 13:33:23 +0800

    Add extension for import file on vercel api

--- api/hello.ts
+++ api/hello.ts
@@ -1,4 +1,4 @@
-import { defineJSONHandler } from '#svr/utils/defineHandler'
+import { defineJSONHandler } from '#svr/utils/defineHandler.js'
 
 export default defineJSONHandler(function () {
   return { name: 'John Doe' }
